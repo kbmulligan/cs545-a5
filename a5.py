c2e1f7ec29a5554c3f63736bbce5fd5cc8cfdccc
nitor <brett.mulligan@gmail.com>
2015-11-14 22:46:14 -0800

    Added cross-val of different SVMs. Currently adding k-subsample method.

--- a5.py
+++ a5.py
@@ -15,6 +15,7 @@ from sklearn import cross_validation, svm, metrics, datasets
 from sklearn import pipeline
 from sklearn import preprocessing as pp
 from sklearn import decomposition as decomp
+from sklearn import feature_selection as fs
 
 arcene_data_filename = 'arcene_tv.data'
 arcene_labels_filename =  'arcene_tv.labels'
@@ -153,7 +154,7 @@ def test_golub (X_arc, y_arc, X_leu, y_leu) :
 
     ############# COMPUTATIONAL EXPENSIVE --- COMMENT OUT UNTIL END #####################
 
-    if complete = True:
+    if complete == True:
         print '\nCalculating Golub scores...'
         golubs_arc = golub(X_arc, y_arc)[0]
         print ''
@@ -173,7 +174,7 @@ def test_golub (X_arc, y_arc, X_leu, y_leu) :
 
 def get_nonzeros (X,y):
 
-    iterations = 5
+    iterations = 1
     nonzeros = []
     for i in range(iterations):
 
@@ -191,6 +192,8 @@ def get_nonzeros (X,y):
 def test_svm (Xa, ya, Xl, yl) :
 
     folds = 5
+    step_delta = 0.05
+
 
     print '\nTesting L1 SVM...'
 
@@ -200,41 +203,98 @@ def test_svm (Xa, ya, Xl, yl) :
     leu_nnz = get_nonzeros(Xl, yl)
     print 'LEUKEMIA - Average number of non-zero elements:', leu_nnz
 
+    
 
+    # setup cross validation folds
+    cross_val = cross_validation.StratifiedKFold(ya, folds, shuffle=True)
 
     svm_l1 = svm.LinearSVC(penalty='l1', dual=False)
     svm_l2 = svm.LinearSVC(penalty='l2', dual=False)
 
-    rfe = RFECV()
+    rfe = fs.RFECV(svm_l2, step=step_delta, cv=folds)
 
     # setup pipes and estimators
-    estimators1 = [('svm1', svm_l1)]
-    estimators2 = [('svm1', svm_l1), ('svm2', svm_l2)]
-    estimators3 = [('svm2', svm_l2)]
-    estimators4 = [('svm1', svm_l1)]
+    estimators0 = [('svm1', svm_l1)]
+    estimators1 = [('svm1', svm_l1), ('svm2', svm_l2)]
+    estimators2 = [('svm2', svm_l2)]
+    estimators3 = [('rfe', rfe)]
 
-    pipe1 = pipeline.Pipeline(estimators1)
+    pipes = [pipeline.Pipeline(estimators0), pipeline.Pipeline(estimators1), pipeline.Pipeline(estimators2), pipeline.Pipeline(estimators3)]
 
-    
-    # setup cross validation folds
-    cross_val = cross_validation.StratifiedKFold(ya, folds, shuffle=True)
+    if complete == True:
+        print '\nARCENE RESULTS................'
+        for pipe in pipes:
 
-    # FIT
-    classifier = pipe1.fit(Xa, ya)
+            # CHECK SCORES
+            cv_scores = cross_validation.cross_val_score(pipe, Xa, ya, cv=folds)
 
+            print 'Cross val scores for', folds, 'folds:'
+            print np.mean(cv_scores), cv_scores 
 
-    # CHECK SCORES
-    cv_scores = cross_validation.cross_val_score(pipe1, Xa, ya, cv=cross_val)
+        print '\nLEUKEMIA RESULTS................'
+        for pipe in pipes:
 
-    print 'Cross val scores for', folds, 'folds:'
-    print cv_scores, np.mean(cv_scores)
+            # CHECK SCORES
+            cv_scores = cross_validation.cross_val_score(pipe, Xl, yl, cv=folds)
 
+            print 'Cross val scores for', folds, 'folds:'
+            print np.mean(cv_scores), cv_scores
 
-
-    
+        print 'Cross validation for SVMs complete!'
 
     return
 
+def test_subsample_method(Xa, ya, Xl, yl):
+
+    Xs, ys = k_subsamples(Xa, ya, 5)
+
+    for X, y in zip(Xs, ys):
+        train_svm(X, y)
+
+    return
+
+
+def k_subsamples(X, y, k):
+
+    X_subs = []
+    y_subs = []
+
+    for i in range(k):
+        newX, newy = rand_subsample(X, y)
+        X_subs.append(newX)
+        y_subs.append(newy)
+
+    assert(len(X_subs) == k)
+    assert(len(y_subs) == k)
+
+    return X_subs, y_subs
+
+def rand_subsample(X, y, frac=0.8):
+    """ Returns random subsample X_sub, y_sub with only fraction 'frac' of the original examples."""
+
+    assert(frac <= 1.0)
+
+    total = len(X)
+
+    keep = int(np.floor(total * frac))
+
+    X_sub = X[:]
+    y_sub = y[:]
+
+    np.random.shuffle(X_sub)
+    np.random.shuffle(y_sub)
+
+    X_sub = X_sub[:keep]
+    y_sub = y_sub[:keep]
+
+    return X_sub, y_sub
+
+
+def train_svm(X,y):
+
+    svm_l1 = svm.LinearSVC(penalty='l1', dual=False)
+    classifier = svm_l1.fit(X,y)
+    return 
 
 if __name__ == '__main__':
     print 'Testing...a5.py'
@@ -249,8 +309,8 @@ if __name__ == '__main__':
     test_golub(X_arc, y_arc, X_leu, y_leu)
 
 
-
     # do L1 svm
     test_svm(X_arc, y_arc, X_leu, y_leu)
     
+    test_subsample_method(X_arc, y_arc, X_leu, y_leu)
 
